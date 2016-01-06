979bde394be8f4fcb803e4394d26a649b53e2861
1206forever <boy120696@bk.ru>
2016-01-06 23:58:31 +0500

    Create cm.mk

---
+++ cm.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := device/samsung/jlte
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/jlte/jlte.mk)
+
+PRODUCT_NAME := cm_jlte
+PRODUCT_DEVICE := jlte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := J100FN
+PRODUCT_MANUFACTURER := samsung
