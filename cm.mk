ab175a9734be70942ec39535f56212e4443e4572
1206forever <boy120696@bk.ru>
2016-01-07 11:36:50 +0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,12 @@
-LOCAL_PATH := device/samsung/jlte
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+# Inherit from these products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/jlte/jlte.mk)
